ec14066f454ae8fd20e7488996f452e4f6e0c39b
Abdelrahman Zahran <101503244+Abdelrahman-Zahran-960@users.noreply.github.com>
2023-11-15 16:47:46 +0300

    Add files via upload

---
+++ Iteration 1/code/CSE3063GRP12/src/features/StudentMenu/TranscriptController.java
@@ -0,0 +1,42 @@
+public class TranscriptController {
+
+    private TranscriptView transcriptView;
+    private TranscriptRepository transcriptRepository;
+
+    public TranscriptController() {
+        this.transcriptView = new TranscriptView();
+        this.transcriptRepository = new TranscriptRepository(this);
+    }
+
+    private Transcript fetchTranscript(String transcriptId) {
+        // Placeholder implementation -> fetch transcript
+        return transcriptRepository.getTranscript(transcriptId);
+    }
+
+    private void navigateToMenu() {
+        // Placeholder implementation -> navigate to the menu
+        transcriptView.displayMenu();
+    }
+
+    private void getUserInput() {
+        // Placeholder implementation -> get user input
+        String userInput = transcriptView.getUserInput();
+        // Process user input as needed
+    }
+
+    private void handleTranscript() {
+        // Placeholder implementation -> handle transcript
+        String transcriptId = getUserInput();
+        Transcript transcript = fetchTranscript(transcriptId);
+        if (transcript != null) {
+            transcriptView.displayTranscript(transcript);
+        } else {
+            transcriptView.displayErrorMessage("Transcript not found.");
+        }
+    }
+
+    public static void main(String[] args) {
+        TranscriptController controller = new TranscriptController();
+        controller.handleTranscript();
+    }
+}
